b7eb85c42723299331607dc3978d2e279dab4e42
jerry hung <jerry110160@gmail.com>
2017-09-24 22:39:32 +0000

    init commit

---
+++ client/js/app.js
@@ -0,0 +1,8 @@
+console.log("Starting app.js");
+const notes = require('./notes.js');
+
+
+console.log('Adiing result is', notes.add(9,-2));
+// var ret = notes.addNotes();
+
+// console.log(`Action is : ${ret}`)
---
+++ client/js/notes.js
@@ -0,0 +1,13 @@
+console.log("Starting notes.js");
+
+module.exports.addNotes= ()=>
+{
+    console.log("Addind notes");
+    return 'New Note!!'
+}
+
+module.exports.add= (a,b)=>
+{
+    console.log("Addind");
+    return a+b;
+}
